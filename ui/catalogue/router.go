a28e47f2b04222c1a647a72b0609d88f45a386ad
andy <andy@sammalmaa.fi>
2025-03-16 23:29:44 +0200

    catalogue: implement fairly stupid search

--- ui/catalogue/router.go
+++ ui/catalogue/router.go
@@ -11,8 +11,34 @@ import (
 func RegisterHandlers(ctx context.Context, config *config.Config, mux *http.ServeMux, engine *template.TemplateEngine) error {
 
 	mux.HandleFunc("GET /catalogue", func(w http.ResponseWriter, r *http.Request) {
+		dto := map[string]any{
+			"Results": []string{},
+		}
+
+		if err := r.ParseForm(); err != nil {
+			w.Write([]byte(err.Error()))
+			w.WriteHeader(400)
+		}
+
+		if query := r.Form.Get("query"); query != "" {
+			reader, err := storage.Reader(ctx, config.DatabaseFile)
+			if err != nil {
+				w.Write([]byte(err.Error()))
+				w.WriteHeader(500)
+				return
+			}
+			results, err := QueryBooks(ctx, reader, 1000, query)
+			if err != nil {
+				w.Write([]byte(err.Error()))
+				w.WriteHeader(500)
+				return
+			}
+
+			dto["Results"] = results
+		}
+
 		w.Header().Set("Content-Type", "text/html")
-		if err := engine.Render(r.Context(), "catalogue/catalogue.html", nil, w); err != nil {
+		if err := engine.Render(r.Context(), "catalogue/catalogue.html", dto, w); err != nil {
 			w.WriteHeader(500)
 		}
 	})
@@ -31,8 +57,4 @@ func RegisterHandlers(ctx context.Context, config *config.Config, mux *http.Serv
 	})
 
 	return nil
-}
-
-type Book struct {
-	BookId string
 }
