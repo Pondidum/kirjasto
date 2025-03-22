018211717f7778514eb0b9a914f30da6f9419a52
andy <andy@sammalmaa.fi>
2025-03-22 14:48:37 +0200

    ui: add radio button component

--- template/funcs.go
+++ template/funcs.go
@@ -1,6 +1,7 @@
 package template
 
 import (
+	"fmt"
 	"html/template"
 	"strings"
 )
@@ -8,5 +9,29 @@ import (
 var funcs = template.FuncMap(map[string]any{
 	"join": func(sep string, v []string) string {
 		return strings.Join(v, sep)
+	},
+	"ternary": func(condition bool, values ...string) string {
+		if condition {
+			return values[0]
+		}
+		if len(values) > 1 {
+			return values[1]
+		}
+
+		return ""
+	},
+	"dict": func(values ...any) map[string]any {
+		dict := map[string]any{}
+		total := len(values)
+
+		for i := 0; i < total; i += 2 {
+			key := fmt.Sprintf("%v", values[i])
+			if i+1 >= total {
+				dict[key] = ""
+				continue
+			}
+			dict[key] = values[i+1]
+		}
+		return dict
 	},
 })
