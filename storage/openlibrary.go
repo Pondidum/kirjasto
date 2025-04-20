d91da96c7f3fbef5c559681fa8c574b1f518c100
andy <andy@sammalmaa.fi>
2025-04-20 19:42:35 +0300

    storage: search with fts

--- storage/openlibrary.go
+++ storage/openlibrary.go
@@ -1,16 +1,11 @@
 package storage
 
 import (
-	"bytes"
 	"context"
-	"encoding/csv"
+	"database/sql"
 	"encoding/json"
-	"fmt"
-	"io"
 	"kirjasto/tracing"
-	"os/exec"
-	"regexp"
-	"strings"
+	"time"
 
 	"go.opentelemetry.io/otel/attribute"
 )
@@ -20,196 +15,67 @@ type Author struct {
 	Name string
 }
 
-type authorDto struct {
-	Key            string
-	Name           string
-	Revision       int
-	LatestRevision int `json:"latest_revision"`
-}
-
 type Book struct {
-	ID      string
-	Name    string
-	Authors []Author
-	Covers  []int
+	ID       string
+	Created  time.Time
+	Modified time.Time
+	Revision int
+	Title    string
+	Authors  []Author
+	Covers   []int
 }
 
-type bookDto struct {
-	Key            string
-	Title          string
-	Covers         []int
-	Revision       int
-	LatestRevision int `json:"latest_revision"`
-	Authors        []struct {
-		Author struct {
-			Key string
-		}
-	}
-}
-
-const (
-	fieldType = iota
-	fieldId
-	fieldVersion
-	fieldModified
-	fieldJson
-)
-
-var workIdRx = regexp.MustCompile(`^OL\d*W$`)
-
-func FindBookByTitle(ctx context.Context, query string) ([]Book, error) {
+func FindBookByTitle(ctx context.Context, reader *sql.DB, term string) ([]Book, error) {
 	ctx, span := tr.Start(ctx, "find_book_by_title")
 	defer span.End()
 
-	span.SetAttributes(attribute.String("query", query))
+	span.SetAttributes(attribute.String("term", term))
 
-	cmd := exec.Command("rg", "--fixed-strings", query, ".data/openlibrary/ol_dump_works_2025-02-11.txt")
+	query := `
+		select
+			w.id,
+			w.created,
+			w.modified,
+			w.revision,
+			w.title,
+			(
+				select json_group_array(json_object('id', a.id, 'name', a.name))
+				from json_each(w.authors) j
+				join authors a on a.id == json_extract(j.value, '$.key')
+			) as 'authors',
+			w.covers,
+			highlight(works_fts, 1, '{{', '}}') as 'match'
+		from works_fts fts
+		join works w on w.id = fts.id
+		where fts.title match @term
+	`
 
-	stdout := &bytes.Buffer{}
-	stderr := &bytes.Buffer{}
-
-	cmd.Stdout = stdout
-	cmd.Stderr = stderr
-
-	err := cmd.Run()
+	rows, err := reader.QueryContext(ctx, query, sql.Named("term", term))
 	if err != nil {
-		if exitErr, ok := err.(*exec.ExitError); ok {
-			if exitErr.ExitCode() == 1 {
-				span.SetAttributes(attribute.Int("search.results_count", 0))
-				return []Book{}, nil
-			}
-		}
-
-		return nil, tracing.Errorf(span, "err: %w\nstdout:%s\nstderr:%s", err, stdout.String(), stderr.String())
+		return nil, tracing.Error(span, err)
 	}
 
-	reader := csv.NewReader(stdout)
-	reader.Comma = '\t'
-	reader.LazyQuotes = true
-
-	idMatch := workIdRx.MatchString(query)
-
-	authorIds := map[string]bool{}
 	books := []Book{}
-	for {
+	for rows.Next() {
+		book := Book{}
+		var authorsJson []byte
+		var coversJson []byte
+		var match string
 
-		line, err := reader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
+		if err := rows.Scan(&book.ID, &book.Created, &book.Modified, &book.Revision, &book.Title, &authorsJson, &coversJson, &match); err != nil {
 			return nil, tracing.Error(span, err)
 		}
 
-		dto := bookDto{}
-		if err := json.Unmarshal([]byte(line[fieldJson]), &dto); err != nil {
-			return nil, tracing.Errorf(span, "error parsing %s: %w", line[fieldId], err)
+		if err := json.Unmarshal([]byte(coversJson), &book.Covers); err != nil {
+			return nil, tracing.Error(span, err)
 		}
 
-		// double check it was a title match
-		if idMatch {
-			if !strings.EqualFold(dto.Key, "/works/"+query) {
-				continue
-			}
-		} else {
-			if !strings.Contains(dto.Title, query) {
-				continue
-			}
-		}
-
-		book := Book{
-			ID:     dto.Key,
-			Name:   dto.Title,
-			Covers: dto.Covers,
-		}
-
-		for _, author := range dto.Authors {
-			authorIds[author.Author.Key] = true
-			book.Authors = append(book.Authors, Author{
-				ID: author.Author.Key,
-			})
+		if err := json.Unmarshal(authorsJson, &book.Authors); err != nil {
+			return nil, tracing.Error(span, err)
 		}
 
 		books = append(books, book)
 	}
 
-	span.SetAttributes(attribute.Int("search.results_count", len(books)))
-
-	authors, err := GetAllAuthors(ctx, authorIds)
-	if err != nil {
-		return books, tracing.Error(span, err)
-	}
-
-	for _, book := range books {
-
-		ba := make([]Author, 0, len(book.Authors))
-		for _, a := range book.Authors {
-			if full, found := authors[a.ID]; found {
-				ba = append(ba, full)
-			}
-		}
-		book.Authors = ba
-	}
-
 	return books, nil
-}
-
-func GetAllAuthors(ctx context.Context, ids map[string]bool) (map[string]Author, error) {
-	ctx, span := tr.Start(ctx, "get_all_authors")
-	defer span.End()
-
-	args := make([]string, 0, (len(ids)*2)+2)
-	args = append(args, "--fixed-strings")
-
-	for id := range ids {
-		args = append(args, "-e", id)
-	}
-	args = append(args, ".data/openlibrary/ol_dump_authors_2025-02-11.txt")
-
-	cmd := exec.Command("rg", args...)
-
-	stdout := &bytes.Buffer{}
-	stderr := &bytes.Buffer{}
-
-	cmd.Stdout = stdout
-	cmd.Stderr = stderr
-
-	if err := cmd.Run(); err != nil {
-		return nil, fmt.Errorf(stderr.String(), err)
-	}
-
-	reader := csv.NewReader(stdout)
-	reader.Comma = '\t'
-	reader.LazyQuotes = true
-
-	authors := make(map[string]Author, len(ids))
-	for {
-		line, err := reader.Read()
-		if err == io.EOF {
-			return authors, nil
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		author, err := authorFromJson([]byte(line[fieldJson]))
-		if err != nil {
-			span.RecordError(err)
-			continue
-		}
-
-		authors[author.ID] = author
-	}
-}
-
-func authorFromJson(content []byte) (Author, error) {
-	dto := authorDto{}
-	if err := json.Unmarshal(content, &dto); err != nil {
-		return Author{}, fmt.Errorf("error parsing %w\n%s", err, string(content))
-	}
-
-	return Author{
-		ID:   dto.Key,
-		Name: dto.Name,
-	}, nil
 }
