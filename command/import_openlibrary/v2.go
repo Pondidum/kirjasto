e0c36c7eb6bd87da9558ec647463e744d19fade5
andy <andy@sammalmaa.fi>
2025-05-02 23:54:00 +0300

    import: authors

--- command/import_openlibrary/v2.go
+++ command/import_openlibrary/v2.go
@@ -3,9 +3,14 @@ package import_openlibrary
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
 	"kirjasto/config"
 	"kirjasto/storage"
 	"kirjasto/tracing"
+	"os"
+	"path"
 
 	"github.com/spf13/pflag"
 )
@@ -32,11 +37,11 @@ func (c *ImportV2Command) Execute(ctx context.Context, config *config.Config, ar
 	ctx, span := tr.Start(ctx, "execute")
 	defer span.End()
 
-	// if len(args) != 1 {
-	// 	return tracing.Errorf(span, "this command takes exactly 1 argument: a path to import")
-	// }
+	if len(args) != 1 {
+		return tracing.Errorf(span, "this command takes exactly 1 argument: a path to import")
+	}
 
-	// dir := args[0]
+	dir := args[0]
 
 	writer, err := storage.Writer(ctx, config.DatabaseFile)
 	if err != nil {
@@ -44,6 +49,16 @@ func (c *ImportV2Command) Execute(ctx context.Context, config *config.Config, ar
 	}
 
 	if err := c.createTables(ctx, writer); err != nil {
+		return tracing.Error(span, err)
+	}
+
+	authorsFile, err := os.Open(path.Join(dir, "ol_dump_authors_2025-02-11.txt"))
+	if err != nil {
+		return tracing.Error(span, err)
+	}
+	defer authorsFile.Close()
+
+	if err := c.populateAuthors(ctx, writer, authorsFile); err != nil {
 		return tracing.Error(span, err)
 	}
 
@@ -89,6 +104,85 @@ func (c *ImportV2Command) createTables(ctx context.Context, writer *sql.DB) erro
 			return tracing.Error(span, err)
 		}
 	}
+
+	return nil
+}
+
+func (c *ImportV2Command) populateAuthors(ctx context.Context, writer *sql.DB, authorsFile io.Reader) error {
+	ctx, span := tr.Start(ctx, "populate_authors")
+	defer span.End()
+
+	tx, err := writer.BeginTx(ctx, &sql.TxOptions{})
+	if err != nil {
+		return tracing.Error(span, err)
+	}
+	defer tx.Rollback()
+
+	authorsStatement := `
+	insert into
+		authors (id, data)
+		values  (@id, @data)
+	on conflict(id) do update set
+		data  = excluded.data
+	`
+
+	statement, err := tx.PrepareContext(ctx, authorsStatement)
+	if err != nil {
+		return tracing.Error(span, err)
+	}
+	defer statement.Close()
+
+	fmt.Println("Populating authors...")
+
+	count := int64(0)
+	for author, err := range iterateFile(authorsFile) {
+		if err != nil {
+			return tracing.Error(span, err)
+		}
+
+		record := &Record{}
+		if err := json.Unmarshal(author, record); err != nil {
+			return tracing.Error(span, err)
+		}
+
+		result, err := statement.ExecContext(ctx,
+			sql.Named("id", record.Key),
+			sql.Named("data", author),
+		)
+		if err != nil {
+			return err
+		}
+		rows, err := result.RowsAffected()
+		if err != nil {
+			return tracing.Error(span, err)
+		}
+		count += rows
+
+		if count%10000 == 0 {
+			fmt.Print(".")
+		}
+	}
+
+	fmt.Println()
+	fmt.Println("Creating FTS table")
+
+	ftsStatement := `
+	delete from authors_fts;
+
+	insert into authors_fts(author_id, name)
+	select id, data ->> '$.name'
+	from authors
+	`
+	if _, err := tx.ExecContext(ctx, ftsStatement); err != nil {
+		return tracing.Error(span, err)
+	}
+
+	fmt.Println("Committing")
+	if err := tx.Commit(); err != nil {
+		return tracing.Error(span, err)
+	}
+
+	fmt.Printf("Done")
 
 	return nil
 }
