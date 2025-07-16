e04ebddd60a34a36ef2245ef8c19aebbc374fcc7
andy <andy@sammalmaa.fi>
2025-07-16 14:55:48 +0300

    goes: return `errNotFound` when trying to load non-existing aggregates

--- goes/store.go
+++ goes/store.go
@@ -3,20 +3,30 @@ package goes
 import (
 	"context"
 	"kirjasto/tracing"
+
+	"go.opentelemetry.io/otel/attribute"
 )
 
 func Load(ctx context.Context, store *SqliteStore /* change to something more generic later*/, state *AggregateState) error {
 	ctx, span := tr.Start(ctx, "load")
 	defer span.End()
 
+	count := 0
 	for event, err := range store.Load(ctx, state.ID()) {
 		if err != nil {
 			return tracing.Error(span, err)
 		}
 
+		count++
+
 		if err := state.ReplayEvent(event); err != nil {
 			return err
 		}
+	}
+
+	span.SetAttributes(attribute.Int("event.count", count))
+	if count == 0 {
+		return ErrNotFound
 	}
 
 	return nil
